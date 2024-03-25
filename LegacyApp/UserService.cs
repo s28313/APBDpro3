a8406e2a9cb0dd92ffc568309ec0db566c1a84b3
s28313 <s28313@pjwstk.edu.pl>
2024-03-25 13:15:25 +0100

    Refractored

---
+++ LegacyApp/UserService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public class UserService
+    {
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || !email.Contains("@") || !email.Contains("."))
+                return false;
+
+            var client = new ClientRepository().GetById(clientId);
+            var user = CreateUser(firstName, lastName, email, dateOfBirth, client);
+            SetCreditDetails(user, client);
+
+            if (user.HasCreditLimit && user.CreditLimit < 500)
+                return false;
+
+            UserDataAccess.AddUser(user);
+            return true;
+        }
+
+        private int CalculateAge(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day))
+                age--;
+            return age;
+        }
+
+        private User CreateUser(string firstName, string lastName, string email, DateTime dateOfBirth, Client client)
+        {
+            return new User
+            {
+                Client = client,
+                DateOfBirth = dateOfBirth,
+                EmailAddress = email,
+                FirstName = firstName,
+                LastName = lastName
+            };
+        }
+
+        private void SetCreditDetails(User user, Client client)
+        {
+            using (var userCreditService = new UserCreditService())
+            {
+                int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                if (client.Type != "VeryImportantClient")
+                    creditLimit *= client.Type == "ImportantClient" ? 2 : 1;
+                user.HasCreditLimit = client.Type != "VeryImportantClient";
+                user.CreditLimit = creditLimit;
+            }
+        }
+    }
+}
